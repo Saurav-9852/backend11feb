06da15a0b9da5975fe6341c045132a6169337803
Saurav-9852 <saurav68265@gmail.com>
2025-02-12 14:31:48 +0530

    Second commit changes port 8000 to 4000

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const app = express();
+const port = 4000;
 
 app.get('/',(req,res)=>{
     res.send("Hello World");
 });
 
-app.listen(8000,()=>{
-    console.log("Server is running on port 8000");
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
 })
